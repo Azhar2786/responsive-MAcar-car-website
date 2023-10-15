f9120e447fa845417b8dd9298111f0df59f39271
Azhar <darkcoders0101@gmail.com>
2023-10-15 16:26:18 +0530

    writing code for nav menu and home

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,34 @@
 /*=============== SHOW MENU ===============*/
 
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),  
+      navClose = document.getElementById('nav-close');
+
+/* ========= MENU SHOW ============ */
+/* validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu');
+    })
+}
+
+/* ========= MENU HIDDEN ============ */
+/* validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu');
+    })
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+
+const navLink = document.querySelectorAll('.nav__link');
+
+navLink.forEach(e => e.addEventListener('click', () =>{
+    const navMenu =  document.getElementById('nav-menu');
+    // when we click on each link we remove show menu class
+    navMenu.classList.remove('show-menu');
+}))
 
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
